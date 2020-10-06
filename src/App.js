25be5514f5cf6349d4f21d8ba07c7b362fcfee3d
alejandro galaz <alejandrogalaz21@gmail.com>
2020-10-06 13:08:57 -0500

    next question guia de  siguitente pregunta

--- src/App.js
+++ src/App.js
@@ -1,16 +1,31 @@
 import React, { useState } from 'react'
 import { questions } from './questions'
+
 function App() {
   const [currentQuestion, setCurrentQuestion] = useState(0)
   const [showScore, setShowScore] = useState(false)
   const [score, setScore] = useState(0)
+  const [answer, setAnswer] = useState('')
+  const [resume, setResume] = useState([])
 
-  function handleAnswerButtonClick(isCorrect) {
-    if (isCorrect) {
-      setScore(score + 1)
-    }
-    const nextQuestion = currentQuestion + 1
-    if (nextQuestion < questions.length) {
+  function handleAnswerSubmit(answerOption) {
+    // set score
+    if (answerOption.isCorrect) setScore(score + 1)
+    // save answers
+    const answerResult = answerOption.answerText || answer
+    setResume([
+      ...resume,
+      {
+        questionText: questions[currentQuestion].questionText,
+        answer: answerResult,
+        type: questions[currentQuestion].type,
+        isCorrect: answerOption.isCorrect
+      }
+    ])
+    // next question redirection
+    const nextQuestion = answerOption.nextQuestion
+    const finish = questions.length - 1
+    if (nextQuestion < finish) {
       setCurrentQuestion(nextQuestion)
     } else {
       setShowScore(true)
@@ -26,16 +41,35 @@ function App() {
         <div className='question-text'>{questions[currentQuestion].questionText}</div>
       </div>
       <div className='answer-section'>
-        {questions[currentQuestion].answerOptions.map((answerOption, index) => (
-          <button onClick={() => handleAnswerButtonClick(answerOption.isCorrect)}>
-            {answerOption.answerText}
-          </button>
-        ))}
+        {questions[currentQuestion].type === 2 ? (
+          <div>
+            <input
+              name='answer'
+              value={answer}
+              onChange={event => setAnswer(event.target.value)}
+            />
+            <button
+              onClick={() => handleAnswerSubmit(questions[currentQuestion].answerOptions[0])}>
+              next question
+            </button>
+          </div>
+        ) : (
+          questions[currentQuestion].answerOptions.map((answerOption, index) => (
+            <button key={index} onClick={() => handleAnswerSubmit(answerOption)}>
+              {answerOption.answerText}
+            </button>
+          ))
+        )}
       </div>
       {showScore && (
-        <div className='score-section'>
-          You scored {score} out of {questions.length}
-        </div>
+        <>
+          <div className='score-section'>
+            You scored {score} out of {questions.length}
+          </div>
+          <div className='result-preview'>
+            <pre>{JSON.stringify(resume, '', 2)}</pre>
+          </div>
+        </>
       )}
     </div>
   )
--- src/questions.js
+++ src/questions.js
@@ -1,38 +1,132 @@
 export const questions = [
   {
     questionText: 'What is the capital of France?',
+    type: 1,
     answerOptions: [
-      { answerText: 'New York', isCorrect: false },
-      { answerText: 'London', isCorrect: false },
-      { answerText: 'Paris', isCorrect: true },
-      { answerText: 'Dublin', isCorrect: false }
+      { answerText: 'New York', isCorrect: true, nextQuestion: 1 },
+      { answerText: 'London', isCorrect: true, nextQuestion: 2 },
+      { answerText: 'Paris', isCorrect: true, nextQuestion: 3 }
     ]
   },
   {
     questionText: 'Who is CEO of Tesla?',
-    answerOptions: [
-      { answerText: 'Jeff Bezos', isCorrect: false },
-      { answerText: 'Elon Musk', isCorrect: true },
-      { answerText: 'Bill Gates', isCorrect: false },
-      { answerText: 'Tony Stark', isCorrect: false }
-    ]
+    type: 2,
+    answerOptions: [{ answerText: '', isCorrect: true, nextQuestion: 2 }]
   },
   {
     questionText: 'The iPhone was created by which company?',
+    type: 1,
     answerOptions: [
-      { answerText: 'Apple', isCorrect: true },
-      { answerText: 'Intel', isCorrect: false },
-      { answerText: 'Amazon', isCorrect: false },
-      { answerText: 'Microsoft', isCorrect: false }
+      { answerText: 'Apple', isCorrect: true, nextQuestion: 3 },
+      { answerText: 'Intel', isCorrect: true, nextQuestion: 3 },
+      { answerText: 'Amazon', isCorrect: true, nextQuestion: 3 },
+      { answerText: 'Microsoft', isCorrect: true, nextQuestion: 3 }
     ]
   },
   {
     questionText: 'How many Harry Potter books are there?',
+    type: 2,
+    answerOptions: [{ answerText: '', isCorrect: true, nextQuestion: 3 }]
+  }
+]
+
+export const resume = [
+  {
+    questionText: '',
+    answer: '',
+    type: '',
+    isCorrect: null
+  }
+]
+
+export const questionsTwo = [
+  {
+    questionText: 'Organizacion del entrevistador',
+    type: 1,
     answerOptions: [
-      { answerText: '1', isCorrect: false },
-      { answerText: '4', isCorrect: false },
-      { answerText: '6', isCorrect: false },
-      { answerText: '7', isCorrect: true }
+      { answerText: 'ACNUR', isCorrect: true, nextQuestion: 1 },
+      { answerText: 'FM4', isCorrect: true, nextQuestion: 3 },
+      { answerText: 'PCR', isCorrect: true, nextQuestion: 3 },
+      { answerText: 'RET', isCorrect: true, nextQuestion: 3 }
     ]
+  },
+  {
+    questionText: 'Oficina del entrevistador',
+    type: 1,
+    answerOptions: [
+      { answerText: 'BOMEX', isCorrect: true, nextQuestion: 3 },
+      { answerText: 'FO Mty', isCorrect: true, nextQuestion: 3 },
+      { answerText: 'FO Ags', isCorrect: true, nextQuestion: 3 },
+      { answerText: 'FO Palenque', isCorrect: true, nextQuestion: 3 },
+      { answerText: 'Otra Organizacion', isCorrect: true, nextQuestion: 2 }
+    ]
+  },
+  {
+    questionText: 'Especifica',
+    type: 2,
+    answerOptions: [{ answerText: '', isCorrect: true, nextQuestion: 3 }]
+  },
+  {
+    questionText:
+      'Indicar si el caso es participante en un programa para la integración local.',
+    type: 1,
+    answerOptions: [
+      {
+        answerText:
+          'El caso es/será participante en el PFFR de RET (recibe/recibirá apoyos en Acayucan, Palenque, Tapachula o Tenosique).',
+        isCorrect: true,
+        nextQuestion: 4
+      },
+      {
+        answerText:
+          'El caso es participante en el programa de integración de FM4 o ACNUR (recibe/recibirá apoyos en Aguascalientes, Guadalajara, Monterrey, Saltillo, Tijuana u otro espacio de integración).',
+        isCorrect: true,
+        nextQuestion: 4
+      },
+      {
+        answerText: 'El caso recibe/recibirá apoyos de PCR en CDMX.',
+        isCorrect: true,
+        nextQuestion: 4
+      },
+      {
+        answerText:
+          'El caso recibe/recibirá apoyos de integración por parte de una oficina móvil o "roving team".',
+        isCorrect: true,
+        nextQuestion: 4
+      }
+    ]
+  },
+  {
+    questionText:
+      'Indicar si el caso fue reubicado a este lugar (por parte del ACNUR y/o socios) y por cuál motivo.',
+    type: 1,
+    answerOptions: [
+      {
+        answerText: 'Sí, el caso fue reubicado para participar en un programa de integración.',
+        isCorrect: true,
+        nextQuestion: 5
+      },
+      {
+        answerText: 'Sí, el caso fue reubicado por temas de salud.',
+        isCorrect: true,
+        nextQuestion: 5
+      },
+      {
+        answerText: 'Sí, el caso fue reubicado por temas de seguridad.',
+        isCorrect: true,
+        nextQuestion: 5
+      },
+      {
+        answerText: 'Sí, el caso fue reubicado por otra razón.',
+        isCorrect: true,
+        nextQuestion: 5
+      }
+    ]
+  },
+  {
+    questionText:
+      'Indicar número de acompañamiento que esta entrevista representa para el caso.',
+    type: 2,
+    answerOptions: [{ answerText: '', isCorrect: true, nextQuestion: 6 }]
   }
 ]
