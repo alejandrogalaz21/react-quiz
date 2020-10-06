33565be22377af5eed8fd2819a1aaf3db6971b8e
alex <jesus.galaz@softtek.com>
2020-10-05 21:20:39 -0500

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,44 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-
+import React, { useState } from 'react'
+import { questions } from './questions'
 function App() {
+  const [currentQuestion, setCurrentQuestion] = useState(0)
+  const [showScore, setShowScore] = useState(false)
+  const [score, setScore] = useState(0)
+
+  function handleAnswerButtonClick(isCorrect) {
+    if (isCorrect) {
+      setScore(score + 1)
+    }
+    const nextQuestion = currentQuestion + 1
+    if (nextQuestion < questions.length) {
+      setCurrentQuestion(nextQuestion)
+    } else {
+      setShowScore(true)
+    }
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <div className='question-section'>
+        <div className='question-count'>
+          <span>Question {currentQuestion + 1}</span>/{questions.length}
+        </div>
+        <div className='question-text'>{questions[currentQuestion].questionText}</div>
+      </div>
+      <div className='answer-section'>
+        {questions[currentQuestion].answerOptions.map((answerOption, index) => (
+          <button onClick={() => handleAnswerButtonClick(answerOption.isCorrect)}>
+            {answerOption.answerText}
+          </button>
+        ))}
+      </div>
+      {showScore && (
+        <div className='score-section'>
+          You scored {score} out of {questions.length}
+        </div>
+      )}
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
